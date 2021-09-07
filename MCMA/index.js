7b2583f33a913de205bf4946788919e80a925061
Gaston Albornoz <gagi126@gmail.com>
2021-09-07 12:57:18 -0300

    Codigo Base
    Codigo Base

---
+++ MCMA/index.js
@@ -0,0 +1,68 @@
+const { response } = require('express')
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+const http = require('http')
+let machines = 
+[{
+    "id":1,
+    "machine":"Pontiac",
+    "tipo_caldera":"Millwright"
+},
+{
+    "id":2,
+    "machine":"Ford",
+    "tipo_caldera":"Boilermaker"
+},
+{
+    "id":3,
+    "machine":"Mercedes-Benz",
+    "tipo_caldera":"Cement Mason"
+}]
+app.get('/',(request, response) =>  {
+    response.send('<h1>MCMA</h1>')
+
+})
+app.get('/api/getmachines',(request,response) =>{
+    response.json(machines)
+})
+app.get('/api/machine/:id',(request,response) => {
+    const id = Number(request.params.id)
+    const machine = machines.find(machine => machine.id == id)
+    console.log({id})
+    console.log({machine})
+    if (machine){
+        response.json(machine)
+    } else{
+        response.status(404).end()
+    }
+})
+app.delete('/api/machine/:id',(request,response) =>{
+    const id = Number(request.params.id)
+    const machine = machines.find(machine => machine.id !== id)
+    response.status(204).end()
+
+})
+app.post('/api/machine',(request,response)=>{
+    const machine = request.body
+    if(!machine || !machine.machine){
+        return response.status(400).json({
+        error: 'machine.machine vacio'
+        })
+    }
+    const ids = machines.map(machine => machine.id)
+    const maxId = Math.max(...ids)
+    const newMachine ={
+        id: maxId + 1,
+        machine: machine.machine,
+        tipo_caldera: machine.tipo_caldera
+    }
+    machines = machines.concat(newMachine)
+    response.json(newMachine)
+})
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
