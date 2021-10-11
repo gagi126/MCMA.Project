e1be8fbc75e2061c45106872fb11d9e79cb52015
Jere Cepeda <jere.derc93@gmail.com>
2021-10-11 20:53:30 -0300

    editUser added

--- MCMA/routes/users/index.js
+++ MCMA/routes/users/index.js
@@ -7,7 +7,8 @@ const jsonParser = bodyParser.json()
 rout.get('/showall_user',jsonParser,controller.indexClients);
 rout.post('/addUser',jsonParser, controller.addUser);
 rout.get('/:_id',jsonParser,controller.getUserById);
-rout.delete('/:_id',jsonParser,controller.deleteUser)
+rout.delete('/:_id',jsonParser,controller.deleteUser);
+rout.put('/:_id',jsonParser,controller.editUser);
 
 rout.post('/addrol',jsonParser, controller.addRol);
 rout.get('/showall_rol',jsonParser,controller.indexRol);
