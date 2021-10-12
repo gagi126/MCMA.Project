f68d5da4f92b8e5e837395e71d04e62678a974c8
Jere Cepeda <jere.derc93@gmail.com>
2021-10-11 22:19:09 -0300

    Prepare Deploy

--- MCMA/index.js
+++ MCMA/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = module.exports = express()
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 const router = require('./routes')
 
 //conection DB
